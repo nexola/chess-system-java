1ebdac5b6b8f06967db43c89c1bdc7a26fcfee84
Vitor Vianna <vitormatheusfv@gmail.com>
2023-04-01 13:49:06 -0300

    First class Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,35 @@
+package boardgame;
+
+public class Position {
+
+    private int row;
+    private int column;
+
+    // Constructor
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    // Getters and setters
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+        return row + ", " + column;
+    }
+}
