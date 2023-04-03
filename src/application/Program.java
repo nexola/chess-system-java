9704c58aff217e0981c748793904d1d1de45eeeb
Vitor Vianna <vitormatheusfv@gmail.com>
2023-04-03 15:24:53 -0300

    Special move promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,6 +6,7 @@ import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -21,31 +22,46 @@ public class Program {
 
         while (!chessMatch.getCheckMate()) {
             try {
+
+                // Clears the screen
                 UI.clearScreen();
+
+                // Prints the match
                 UI.printMatch(chessMatch, captured);
+
+                // Asks and performs a move
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
-
                 boolean[][] possibleMoves = chessMatch.possibleMoves(source);
                 UI.clearScreen();
                 UI.printBoard(chessMatch.getPieces(), possibleMoves);
-
                 System.out.println();
                 System.out.print("Target: ");
                 ChessPosition target = UI.readChessPosition(sc);
 
+                // Captures the piece if so
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 
+                // Adds the piece to the list of captured pieces
                 if (capturedPiece != null) {
                     captured.add(capturedPiece);
                 }
-            } catch (ChessException | InputMismatchException e) {
+
+                // Case promoted
+                if (chessMatch.getPromoted() != null) {
+                    System.out.print("Enter piece for promotion (B/C/R/Q): ");
+                    String type = sc.nextLine();
+                    chessMatch.replacePromotedPiece(type);
+                }
+                // Exception treatment
+            } catch (ChessException | InputMismatchException | InvalidParameterException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
             }
         }
         UI.clearScreen();
+        // Prints the final state of match
         UI.printMatch(chessMatch, captured);
     }
 }
